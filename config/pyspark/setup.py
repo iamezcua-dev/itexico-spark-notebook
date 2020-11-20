bb258d4c1ba2f554b0239c0e5b3af0161f3ec3c1
Isaac Amezcua <isaac.amezcua@improving.com>
2020-11-20 12:57:36 -0600

    Delete useless kernel setup snippet

--- config/pyspark/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-os.environ["PYSPARK_PYTHON"] = "/opt/conda/bin/python"
-os.environ["PYLIB"] = os.environ["SPARK_HOME"] + "/python/lib"
-sys.path.insert(0, os.environ["PYLIB"] +"/py4j-0.10.9-src.zip")
-sys.path.insert(0, os.environ["PYLIB"] +"/pyspark.zip")
-os.environ["PYSPARK_SUBMIT_ARGS"] = "--name yarn pyspark-shell"
