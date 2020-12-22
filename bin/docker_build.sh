d93a3fec1a46cfb725f05cff13ee9eff26bacca8
Isaac Eduardo Amezcua Bustos <iamezcua.dev@gmail.com>
2020-12-21 23:31:26 -0600

    Remove unused Docker image building script
    - Now Docker Compose is directly used to build the images

--- bin/docker_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LOG_FOLDER="logs"
-LIVY_CONTAINER_NAME="itexico-livy-server"
-JUPYTER_CONTAINER_NAME="itexico-jupyter-notebook"
-
-docker build --tag $LIVY_CONTAINER_NAME:latest -f Dockerfile.livy .
-docker build --tag $JUPYTER_CONTAINER_NAME:latest -f Dockerfile.jupyter .
